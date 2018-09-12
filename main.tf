421eaa23bd7b90cc8cbd087794fe1bc05c48fd86
Wander Lairson Costa <wcosta@mozilla.com>
2018-09-12 17:33:05 -0300

    Create a grid of instances

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ provider "google" {
   region = "us-east-1"
 }
 
-resource "google_compute_instance_template" "docker_worker" {
-  name = "docker-worker"
+resource "google_compute_instance_template" "std2" {
+  name = "docker-worker-std2"
 
   labels = {
-    worker = "docker-worker"
+    worker = "docker-worker-std2"
   }
 
   instance_description = "docker-worker"
-  machine_type = "custom-4-26624"
+  machine_type = "n1-standard-2"
   can_ip_forward = false
 
   scheduling {
@@ -45,13 +45,13 @@ resource "google_compute_instance_template" "docker_worker" {
     clientId = "${var.client_id}"
     accessToken = "${var.access_token}"
     capacity = "4"
-    workerType = "gecko-t-linux"
+    workerType = "n1-std-2"
     provisionerId = "gce"
   }
 }
 
-resource "google_compute_instance_group_manager" "docker_worker" {
-  name = "docker-worker-instances"
+resource "google_compute_instance_group_manager" "grp_std2" {
+  name = "docker-worker-instances-std2"
 
   base_instance_name = "docker-worker"
   update_strategy = "NONE"
@@ -59,5 +59,180 @@ resource "google_compute_instance_group_manager" "docker_worker" {
   wait_for_instances = true
   zone = "us-east1-b"
 
-  instance_template = "${google_compute_instance_template.docker_worker.self_link}"
+  instance_template = "${google_compute_instance_template.std2.self_link}"
 }
+
+resource "google_compute_instance_template" "std4" {
+  name = "docker-worker-std4"
+
+  labels = {
+    worker = "docker-worker-std4"
+  }
+
+  instance_description = "docker-worker"
+  machine_type = "n1-standard-4"
+  can_ip_forward = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  disk {
+    source_image = "taskcluster-playground/docker-worker-app-1536593314"
+    auto_delete = true
+    boot = true
+  }
+
+  disk {
+    auto_delete = true
+    boot = false
+    disk_size_gb = 120
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      network_tier = "STANDARD"
+    }
+  }
+
+  metadata = {
+    statelessHostname = "${var.stateless_hostname}"
+    relengApiToken = "${var.relengapi_token}"
+    clientId = "${var.client_id}"
+    accessToken = "${var.access_token}"
+    capacity = "4"
+    workerType = "n1-std-4"
+    provisionerId = "gce"
+  }
+}
+
+resource "google_compute_instance_group_manager" "grp_std4" {
+  name = "docker-worker-instances-std4"
+
+  base_instance_name = "docker-worker"
+  update_strategy = "NONE"
+  target_size = 4
+  wait_for_instances = true
+  zone = "us-east1-b"
+
+  instance_template = "${google_compute_instance_template.std4.self_link}"
+}
+
+resource "google_compute_instance_template" "std8" {
+  name = "docker-worker-std8"
+
+  labels = {
+    worker = "docker-worker-std8"
+  }
+
+  instance_description = "docker-worker"
+  machine_type = "n1-standard-8"
+  can_ip_forward = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  disk {
+    source_image = "taskcluster-playground/docker-worker-app-1536593314"
+    auto_delete = true
+    boot = true
+  }
+
+  disk {
+    auto_delete = true
+    boot = false
+    disk_size_gb = 120
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      network_tier = "STANDARD"
+    }
+  }
+
+  metadata = {
+    statelessHostname = "${var.stateless_hostname}"
+    relengApiToken = "${var.relengapi_token}"
+    clientId = "${var.client_id}"
+    accessToken = "${var.access_token}"
+    capacity = "4"
+    workerType = "n1-std-8"
+    provisionerId = "gce"
+  }
+}
+
+resource "google_compute_instance_group_manager" "grp_std8" {
+  name = "docker-worker-instances-std8"
+
+  base_instance_name = "docker-worker"
+  update_strategy = "NONE"
+  target_size = 4
+  wait_for_instances = true
+  zone = "us-east1-b"
+
+  instance_template = "${google_compute_instance_template.std8.self_link}"
+}
+
+resource "google_compute_instance_template" "std16" {
+  name = "docker-worker-std16"
+
+  labels = {
+    worker = "docker-worker-std16"
+  }
+
+  instance_description = "docker-worker"
+  machine_type = "n1-standard-16"
+  can_ip_forward = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  disk {
+    source_image = "taskcluster-playground/docker-worker-app-1536593314"
+    auto_delete = true
+    boot = true
+  }
+
+  disk {
+    auto_delete = true
+    boot = false
+    disk_size_gb = 120
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      network_tier = "STANDARD"
+    }
+  }
+
+  metadata = {
+    statelessHostname = "${var.stateless_hostname}"
+    relengApiToken = "${var.relengapi_token}"
+    clientId = "${var.client_id}"
+    accessToken = "${var.access_token}"
+    capacity = "4"
+    workerType = "n1-std-16"
+    provisionerId = "gce"
+  }
+}
+
+resource "google_compute_instance_group_manager" "grp_std16" {
+  name = "docker-worker-instances-std16"
+
+  base_instance_name = "docker-worker"
+  update_strategy = "NONE"
+  target_size = 4
+  wait_for_instances = true
+  zone = "us-east1-b"
+
+  instance_template = "${google_compute_instance_template.std16.self_link}"
+}
+
